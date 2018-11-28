898c4304a1c638ef61cbef0eb05ef06f63d4ae9e
me.msyariefh <45348110+personaproject@users.noreply.github.com>
2018-11-28 10:19:57 +0700

    Delete install.sh

--- persona_1.0.5-beta/install.sh
+++
@@ -1,4 +0,0 @@
-sudo cp -i -r persona /usr/share/plymouth/themes/
-sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/persona/persona.plymouth 100
-sudo update-alternatives --config default.plymouth
-sudo update-initramfs -u
